a8b7e609e255abfba75adf85a088391b84b6ca8b
jeremy <jeremywib7@gmail.com>
2022-09-28 16:59:45 +0700

    added currency conversion service route

--- currency-exchange-service/src/main/java/com/fang/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/fang/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,16 +1,12 @@
 package com.fang.microservices.currencyexchangeservice.controllers;
 
 import com.fang.microservices.currencyexchangeservice.models.CurrencyExchange;
-import com.fang.microservices.currencyexchangeservice.repositories.CurrencyExchangeRepository;
 import com.fang.microservices.currencyexchangeservice.services.CurrencyExchangeService;
 import lombok.AllArgsConstructor;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
 
 @RestController
 @AllArgsConstructor
